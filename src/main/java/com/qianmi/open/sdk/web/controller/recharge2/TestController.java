b8b22278e8777ff04925b71cce61ca8279b572d2
lizhi-123 <17608065223@163.com>
2020-12-24 14:35:34 +0800

    web_lixiang分支添加的新功能

--- src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
+++ src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
@@ -1,4 +1,8 @@
 package com.qianmi.open.sdk.web.controller.recharge2;
 
 public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("这是web_lixiang分支开发的新功能");
+    }
 }
