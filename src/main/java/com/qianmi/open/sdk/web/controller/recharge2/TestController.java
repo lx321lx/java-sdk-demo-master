6f1c8759fb08bb82353a39229efea7e35acca9be
lizhi-123 <17608065223@163.com>
2020-12-24 14:40:46 +0800

    web_lixiang分支添加的新功能真正的

--- src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
+++ src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
@@ -1,4 +1,10 @@
 package com.qianmi.open.sdk.web.controller.recharge2;
 
 public class TestController {
+    @Override
+    public int hashCode() {
+
+        /*这个才是web分支添加的新功能*/
+        return super.hashCode();
+    }
 }
