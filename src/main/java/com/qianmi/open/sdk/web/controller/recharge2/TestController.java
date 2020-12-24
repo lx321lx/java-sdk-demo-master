8f5f729ccc27a641310bf485dda04cb5e1f983bb
lizhi-123 <17608065223@163.com>
2020-12-24 15:21:09 +0800

    idea_lixiang

--- src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
+++ src/main/java/com/qianmi/open/sdk/web/controller/recharge2/TestController.java
@@ -11,4 +11,8 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("这是web_lixiang分支开发的新功能");
     }
+
+    public void idea(){
+        System.out.println("idea_Lixiang");
+    }
 }
